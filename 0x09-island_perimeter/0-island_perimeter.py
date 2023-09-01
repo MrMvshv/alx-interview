d4a613cffd8a3945f1a75fb0728a7285fbc15a7e
Irungu Macharia <machariajosphat001@gmail.com>
2023-09-01 12:23:03 +0000

    list indexing and traversal

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,11 +8,29 @@ def island_perimeter(grid):
     for row in grid:
         print("row:")
         print(row)
-        print("row number: " + rowNo)
+        print("row number: ")
+        print(rowNo)
         rowNo += 1
         indexNo = 0
         for item in row:
-            print("item:")
-            print(item)
-            print(indexNo)
+#            print("item:")
+#            print(item)
+#            print(indexNo)
             indexNo += 1
+            if (item == 0):
+                pass
+            else:
+                print("1 item:")
+                print(item)
+                if rowNo < (len(row) - 1):
+                    if (grid[rowNo + 1][indexNo] == 1):
+                        print("down one")
+                if rowNo > 0:
+                    if (grid[rowNo - 1][indexNo] == 1):
+                        print("up one")
+                if indexNo < (len(row) - 1):
+                    if (row[indexNo + 1] == 1):
+                        print("right one")
+                if indexNo > 0:
+                    if (row[indexNo - 1] == 1):
+                        print("left one")
